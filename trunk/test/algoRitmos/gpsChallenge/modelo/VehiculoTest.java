878a90ee3d97fc6101af687aae737f72b0b0f1ab
garrigo <garrigo@360b39a5-2575-14ce-afec-94d8f1905820>
2013-11-18 04:24:50 +0000

    Nueva versión de VehiculoTest

--- trunk/test/algoRitmos/gpsChallenge/modelo/VehiculoTest.java
+++ trunk/test/algoRitmos/gpsChallenge/modelo/VehiculoTest.java
@@ -7,31 +7,40 @@ public class VehiculoTest {
 
 	@Test
 	public void testVehiculoMover() {
-		
+	//Prueba que el vehículo se mueva a la nueva esquina y que se incremente el contador de movimientos
 		Esquina unaEsquina = new Esquina();
 		Esquina unaEsquinaNorte = new Esquina();
 		unaEsquina.setEsquinaAl(new Norte(), unaEsquinaNorte);
-		
+		int movimientos;
 		Vehiculo unVehiculo = new Vehiculo(unaEsquina, new Auto());;
 		
+		movimientos = unVehiculo.getMovimientos();
 		Assert.assertEquals(unVehiculo.getUbicacion(), unaEsquina);
+		Assert.assertEquals(unVehiculo.getMovimientos(), movimientos);
+		
 		unVehiculo.moverA(new Norte());
 		Assert.assertEquals(unVehiculo.getUbicacion(), unaEsquinaNorte);
-		
+		movimientos++;
+		Assert.assertEquals(unVehiculo.getMovimientos(), movimientos);
 	}
 	
 	@Test
 	public void testVehiculoMoverADireccionEsquinaNull() {
-	//Al mover el vehículo hacia una esquina Null, debería quedarse en la misma posición actual
+	//Al mover el vehículo hacia una esquina Null, debería quedarse en la misma 
+	//posición actual y no incrementar el contador de movimientos
 		Esquina unaEsquina = new Esquina();
 		Esquina unaEsquinaNull = null;
 		unaEsquina.setEsquinaAl(new Norte(), unaEsquinaNull);
-		
+		int movimientos;
 		Vehiculo unVehiculo = new Vehiculo(unaEsquina, new Auto());;
-		
-		Assert.assertEquals(unVehiculo.getUbicacion(), unaEsquina);
+	
+		movimientos = unVehiculo.getMovimientos();
 		unVehiculo.moverA(new Norte());
 		Assert.assertEquals(unVehiculo.getUbicacion(), unaEsquina);
+		Assert.assertEquals(unVehiculo.getMovimientos(), movimientos);
 		
 	}
+	
+	
+	
 }
