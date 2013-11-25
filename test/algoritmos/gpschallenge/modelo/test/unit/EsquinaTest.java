acb0e68e5e0aab6f116f3278d6fe61d32af2487d
garrigo <garrigo@360b39a5-2575-14ce-afec-94d8f1905820>
2013-11-25 00:12:30 +0000

    Nuevo agregado de pruebas a EsquinaTest. 24/nov/2013 21:12hs

--- test/algoritmos/gpschallenge/modelo/test/unit/EsquinaTest.java
+++ test/algoritmos/gpschallenge/modelo/test/unit/EsquinaTest.java
@@ -4,16 +4,24 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import algoritmos.gpschallenge.modelo.juego.Esquina;
+import algoritmos.gpschallenge.modelo.obstaculo.Pozo;
 import algoritmos.gpschallenge.modelo.direccion.*;
 
 public class EsquinaTest {
 	
 	@Test
 	public void testCrearEsquinaNoNula() {
-		Esquina esquina = new Esquina();
+		Esquina unaEsquina = new Esquina();
 		
-		Assert.assertNotNull(esquina);
+		Assert.assertNotNull(unaEsquina);
 	}
+	
+	@Test
+	public void testCrearEsquinaYVerificarNoTieneElemento() {
+		Esquina unaEsquina = new Esquina();
+		Assert.assertNull(unaEsquina.getElementoEnEsquina());
+	}
+	
 	
 	@Test
 	public void testCrearEsquinaYChequearQueEsteAislada() {
@@ -33,4 +41,53 @@ public class EsquinaTest {
 		esquina.setEsquinaAl(new Norte(), esquinaNorte);
 		Assert.assertNotNull(esquina.getEsquinaAl(new Norte()));
 	}
+	
+	@Test
+	public void testAgregarElementoEnEsquina() {
+		Esquina unaEsquina = new Esquina();
+		Pozo unObstaculo = new Pozo(); 
+
+		unaEsquina.addElementoEnEsquina(unObstaculo);
+		Assert.assertEquals(unObstaculo, unaEsquina.getElementoEnEsquina());
+	}
+	
+	@Test
+	public void testCrearEsquinacConEsquinasProximasNoNulas() {
+		Esquina unaEsquina = new Esquina();
+		Esquina unaEsquinaNorte = new Esquina();
+		Esquina unaEsquinaSur = new Esquina();
+		Esquina unaEsquinaOeste = new Esquina();
+		Esquina unaEsquinaEste = new Esquina();
+		
+		unaEsquina.setEsquinaAl(new Norte(), unaEsquinaNorte);
+		unaEsquina.setEsquinaAl(new Sur(), unaEsquinaSur);
+		unaEsquina.setEsquinaAl(new Oeste(), unaEsquinaOeste);
+		unaEsquina.setEsquinaAl(new Este(), unaEsquinaEste);
+		
+		Assert.assertEquals(unaEsquina.getEsquinaAl(new Norte()), unaEsquinaNorte);
+		Assert.assertEquals(unaEsquina.getEsquinaAl(new Sur()), unaEsquinaSur);
+		Assert.assertEquals(unaEsquina.getEsquinaAl(new Oeste()), unaEsquinaOeste);
+		Assert.assertEquals(unaEsquina.getEsquinaAl(new Este()), unaEsquinaEste);
+		
+	}
+	
+	@Test
+	public void testTieneEsquinasProximasNulasYNoNulas() {
+		Esquina unaEsquina = new Esquina();
+		Esquina unaEsquinaNorte = new Esquina();
+		Esquina unaEsquinaSur = new Esquina();
+		//No se agrega una esquina al Oeste
+		//No se agrega una esquina al Este
+		
+		unaEsquina.setEsquinaAl(new Norte(), unaEsquinaNorte);
+		unaEsquina.setEsquinaAl(new Sur(), unaEsquinaSur);
+		
+		Assert.assertEquals(unaEsquina.getEsquinaAl(new Norte()), unaEsquinaNorte);
+		Assert.assertEquals(unaEsquina.getEsquinaAl(new Sur()), unaEsquinaSur);
+		Assert.assertNull(unaEsquina.getEsquinaAl(new Oeste()));
+		Assert.assertNull(unaEsquina.getEsquinaAl(new Este()));
+		
+	}
+	
+	
 }
