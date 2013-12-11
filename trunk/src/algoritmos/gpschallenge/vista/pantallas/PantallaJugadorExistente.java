c9ebc810e067281221adb9ae3d9373595451c218
garrigo@gmail.com <garrigo@gmail.com@360b39a5-2575-14ce-afec-94d8f1905820>
2013-12-11 16:52:20 +0000

    Algunas mejoras en la navegación de las ventanas.

--- trunk/src/algoritmos/gpschallenge/vista/pantallas/PantallaJugadorExistente.java
+++ trunk/src/algoritmos/gpschallenge/vista/pantallas/PantallaJugadorExistente.java
@@ -4,21 +4,36 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+
 import java.awt.Font;
 import java.awt.Color;
+
 import javax.swing.SwingConstants;
+
 import org.eclipse.wb.swing.FocusTraversalOnArray;
+
+import algoritmos.gpschallenge.modelo.juego.Jugador;
+
 import java.awt.Component;
 import java.awt.Toolkit;
+
 import javax.swing.JComboBox;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class PantallaJugadorExistente {
 
-	private JFrame frmGpsChallenge;
+	private JFrame frame;
 	private JTextField txtTitulo;
 	private JButton btnAceptar;
-
+	private PantallaJugadorExistente windowJugadorExistente;
+	private PantallaBienvenida windowBienvenida;
+	private PantallaOpciones windowOpciones;
+	private String nombre;
+	private Jugador jugador;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -27,7 +42,7 @@ public class PantallaJugadorExistente {
 			public void run() {
 				try {
 					PantallaJugadorExistente window = new PantallaJugadorExistente();
-					window.frmGpsChallenge.setVisible(true);
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -40,22 +55,23 @@ public class PantallaJugadorExistente {
 	 */
 	public PantallaJugadorExistente() {
 		initialize();
-		frmGpsChallenge.setVisible(true);
+		windowJugadorExistente = this;
+		frame.setVisible(true);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmGpsChallenge = new JFrame();
-		frmGpsChallenge.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\gaston\\JAVAworkspace\\PruebaMVC\\images\\minicooper64_2.ico"));
-		frmGpsChallenge.setTitle("GPS Challenge");
-		frmGpsChallenge.setName("frameBienvenida");
-		frmGpsChallenge.getContentPane().setBackground(new Color(255, 255, 255));
-		frmGpsChallenge.getContentPane().setForeground(new Color(0, 0, 0));
-		frmGpsChallenge.setBounds(100, 100, 450, 300);
-		frmGpsChallenge.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmGpsChallenge.getContentPane().setLayout(null);
+		frame = new JFrame();
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\gaston\\JAVAworkspace\\PruebaMVC\\images\\minicooper64_2.ico"));
+		frame.setTitle("GPS Challenge");
+		frame.setName("frameBienvenida");
+		frame.getContentPane().setBackground(new Color(255, 255, 255));
+		frame.getContentPane().setForeground(new Color(0, 0, 0));
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
 		txtTitulo = new JTextField();
 		txtTitulo.setRequestFocusEnabled(false);
@@ -67,17 +83,37 @@ public class PantallaJugadorExistente {
 		txtTitulo.setEditable(false);
 		txtTitulo.setText("Elija su usuario");
 		txtTitulo.setBounds(10, 31, 414, 29);
-		frmGpsChallenge.getContentPane().add(txtTitulo);
+		frame.getContentPane().add(txtTitulo);
 		txtTitulo.setColumns(10);
 		
 		btnAceptar = new JButton("Aceptar");
+		btnAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//TODO cargar jugador existente
+				frame.dispose();
+				windowOpciones = new PantallaOpciones(jugador); //Falta enviarle el Jugador como argumento
+				
+			}
+		});
 		btnAceptar.setActionCommand("GuardarNombre");
-		btnAceptar.setBounds(132, 169, 171, 49);
-		frmGpsChallenge.getContentPane().add(btnAceptar);
+		btnAceptar.setBounds(138, 169, 72, 49);
+		frame.getContentPane().add(btnAceptar);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(132, 89, 171, 29);
-		frmGpsChallenge.getContentPane().add(comboBox);
-		frmGpsChallenge.getContentPane().setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{btnAceptar}));
+		frame.getContentPane().add(comboBox);
+		//TODO cargar nombres de jugadores pre-existentes
+		
+		JButton button = new JButton("Volver");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				windowBienvenida = new PantallaBienvenida();
+				frame.dispose();
+			}
+		});
+		button.setAlignmentX(0.5f);
+		button.setBounds(231, 169, 72, 49);
+		frame.getContentPane().add(button);
+		frame.getContentPane().setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{btnAceptar}));
 	}
 }
